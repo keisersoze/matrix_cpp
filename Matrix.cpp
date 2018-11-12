08f02d4fd239bf776c6dbd33a426141c73990d7f
keisersoze <filippomaganza@gmail.com>
2018-11-12 18:24:08 +0100

    Created class Matrix

---
+++ Matrix.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Filippo Maganza on 12/11/2018.
+//
+
+#include "Matrix.h"
+
+template<class E>
+
+int Matrix<E>::getValue() {
+    return 0;
+}
---
+++ Matrix.h
@@ -0,0 +1,17 @@
+//
+// Created by Filippo Maganza on 12/11/2018.
+//
+
+#ifndef MATRIX_CPP_MATRIX_H
+#define MATRIX_CPP_MATRIX_H
+
+
+template <class E> class Matrix {
+public:
+    int val;
+    int getValue();
+
+};
+
+
+#endif //MATRIX_CPP_MATRIX_H

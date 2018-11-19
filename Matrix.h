343422c44a24bfb2d14b38efee512c909420dcad
Unknown <filippomaganza@gmail.com>
2018-11-19 16:45:39 +0100

    constructor + basic acessors

--- Matrix.h
+++ Matrix.h
@@ -5,51 +5,72 @@
 #ifndef MATRIX_CPP_MATRIX_H
 #define MATRIX_CPP_MATRIX_H
 
-#include <vector>
+#include <array>
 #include <iostream>
 #include <memory>
 
 using namespace std;
 
-template <class E, int m1, int n1> class Matrix {
+template <typename E, int r, int n>
+class Matrix {
+
+    static_assert(r > 0, "Brutto deficiente");
+    static_assert(n > 0, "Brutto deficiente");
+
+
 private:
-    class Bad_Input{}; // to throw errors, for example when m1 < 1 
-    shared_ptr < vector < E > > matrix_ptr; //by_arn: explaination needed...?
-
-    bool transposed;    
-    bool diagonalized;
-    int m_;
-    int n_;
-
-    Matrix(const shared_ptr < vector<E> > & matrix_ptr);
+    //class Bad_Input{}; // to throw errors, for example when m1 < 1
+    shared_ptr < array <  E , n * r > > matrix_ptr; //by_arn: explaination needed...?
 
 public:
 
-    Matrix(const E Matrix[m1][n1]);
+    Matrix(const array<E, n * r > &array) {
+        matrix_ptr = std::make_shared<std::array<E, n * r > >(array);
+    }
 
-    virtual ~Matrix();
+    E at (int accessed_row, int accessed_column){
+        array<E, n * r > v = *(matrix_ptr.get());
+        if(accessed_row < 1) {
+            //throelse
+        }else if (accessed_column < 1){
+            //
+        }else
+            return v[ n * (accessed_row - 1) + accessed_column - 1];
+    };
 
-    void setTransposed(bool transposed);        // by_arn: what are these bools implying for implementation? If you use a setter for transposed you'll cause errors
+    E operator()(int accessed_row, int accessed_column){
+        return at(accessed_row,accessed_column);
+    };
 
-    void setDiagonalized(bool diagonalized);
+
+
+
+
+
+
+
+
 
     // Getters
-    int get_row();
 
-    int get_column();
+    //E get(int r_index, int c_index);
 
-    E get(int r_index, int c_index);
-
-    E operator()(int r_index, int c_index);
+    //E operator()(int r_index, int c_index);
 
     //matrix<E> submatrix(int n, int m);
-    Matrix<E, n1, m1> transpose();
-    Matrix<E, m1, n1> diagonal();
-    const Matrix <E, m1, n1> diagonalMatrix();
+    //Matrix<E, n1, m1> transpose();
+    //Matrix<E, m1, n1> diagonal();
+    //const Matrix <E, m1, n1> diagonalMatrix();
 
-    
     //only for testing purpose
-    void print ();
+    void print (){
+        array<int, n * r> array= *(matrix_ptr.get());
+        for (int i = 0; i < n * r; ++i) {
+            std::cout << array[i] << ' ';
+        }
+
+
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 //
 
 #include <iostream>
+#include <memory>
 #include "Matrix.h"
 
+
+
 int main (){
-    int  mymatrix [3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-    Matrix<int, 3, 3> matrix(mymatrix);
+    /*int  mymatrix [3][3] = { { 1, 2, 3 },
+                             { 4, 5, 6 },
+                             { 7, 8, 9 } };*/
+    std::array< int , 4> a2 = {1, 2, 3, 4};
+    std::array< int , 3> a3 = {1, 2, 3};
+    Matrix<int,2,2> matrix(a2);
     matrix.print();
+    cout<<matrix(2,2);
     return 0;
 }

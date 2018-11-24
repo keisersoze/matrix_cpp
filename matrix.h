4dc061a093348a2c0b19e0383af507535fbc5b7c
keisersoze <filippomaganza@gmail.com>
2018-11-24 10:27:20 +0100

    improvement in design

--- matrix.h
+++ matrix.h
@@ -8,26 +8,41 @@
 #include "matrix_impl.h"
 
 template < typename E>
+class matrix_tail{
+private:
+    unique_ptr < matrix_impl <E> > decorated_matrix;
+public:
+    matrix_tail(const unique_ptr<matrix_impl<E>> &decorated_matrix) : decorated_matrix(decorated_matrix) {}
+
+    const unique_ptr<matrix_impl<E>> &getDecorated_matrix() const {
+        return decorated_matrix;
+    }
+
+};
+
+template < typename E>
 class matrix {
 
 private:
 
-    shared_ptr < matrix_impl <E> > wrapped_matrix;
+    shared_ptr < matrix_impl <E> > matrix_tail_instance;
 
 public:
 
-    matrix(const vector<E> &data, int r, int c): wrapped_matrix (new base_matrix_impl<E>(data,r,c)){
+    matrix(const vector<E> &data, int r, int c): matrix_tail_instance (new base_matrix_impl<E>(data,r,c)){
     }
 
     E get(int accessed_row, int accessed_column){
-        return wrapped_matrix -> get(accessed_row,accessed_column);
+        return matrix_tail_instance -> get(accessed_row,accessed_column);
     }
 
     matrix* transpose(){
-        wrapped_matrix.reset(new transposed_matrix_impl <E> (wrapped_matrix));
+        matrix_tail_instance.reset(new transposed_matrix_impl <E> (matrix_tail_instance));
         return this;
     }
 
 };
+
+
 
 #endif //MATRIX_CPP_MATRIX_WRAPPER_H

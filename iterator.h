37b15ac41633cab4179376268649267b8b058cbb
Arn <arnoud.piano@gmail.com>
2018-11-27 18:45:35 +0100

    column iterator

--- iterator.h
+++ iterator.h
@@ -40,3 +40,40 @@ public:
     }
 
 };
+
+template < typename E>
+class column_matrix_iterator{
+
+private:
+
+    shared_ptr < matrix_impl <E> > shared_ptr1;
+    unsigned cr;
+    unsigned cc;
+
+public:
+
+    column_matrix_iterator(shared_ptr<matrix_impl<E>> matrix_ptr, unsigned r, unsigned c)
+            : shared_ptr1(matrix_ptr) , cr(r) , cc(c) {}
+
+    column_matrix_iterator& operator ++(){
+        cr++;
+        if (cr == shared_ptr1->getColumnNumber() + 1) {
+            cr = 1;
+            cc++;
+        }
+        return *this;
+    }
+
+    const E& operator *(){
+        return shared_ptr1->get_ref(cr,cc);
+    }
+
+    bool operator == (const row_matrix_iterator& x){
+        return cc == x.cc && cr == x.cr;
+    }
+
+    bool operator != (const row_matrix_iterator& x){
+        return ! operator == (x);
+    }
+
+};
